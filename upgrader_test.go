d9fb4b9268d65600aa9ef96e07c58b4c3c1d5067
Euan Kemp <euan@ngrok.com>
2019-01-29 10:59:11 -0800

    Add upgrader test

---
+++ upgrader_test.go
@@ -0,0 +1,125 @@
+package tableroll
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/inconshreveable/log15"
+)
+
+var l = log15.New()
+
+func TestUpgradeHandoff(t *testing.T) {
+	coordDir, err := ioutil.TempDir("", "tableroll_test")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(coordDir)
+
+	server1Msgs, server2Msgs := make(chan string), make(chan string)
+	server1Reqs, server2Reqs := make(chan struct{}), make(chan struct{})
+
+	// Server 1 starts listening
+	upg1, s1 := createTestServer(t, 1, coordDir, server1Reqs, server1Msgs)
+	// defer upg1.Stop() // TODO, this hangs :(
+	defer s1.Close()
+	c1 := s1.Client()
+
+	go func() {
+		<-server1Reqs
+		server1Msgs <- "msg1"
+	}()
+	assertResp(t, s1.URL, c1, "msg1")
+	// s1 is listening
+
+	// leave a hanging client connection for s1 before upgrading, then read it after upgrading
+	msg2Response := make(chan struct{})
+	go func() {
+		assertResp(t, s1.URL, c1, "msg2")
+		msg2Response <- struct{}{}
+	}()
+	<-server1Reqs
+
+	// now have s2 take over for s1
+	upg2, s2 := createTestServer(t, 2, coordDir, server2Reqs, server2Msgs)
+	// defer upg2.Stop()
+	_ = upg2
+	<-upg1.Exit()
+	upg1.Stop()
+	s1.Listener.Close() // Should this happen here, or should upg.Fds take care of this on upgrade ready automatically?
+	defer s2.Close()
+	go func() {
+		<-server2Reqs
+		server2Msgs <- "msg3"
+	}()
+	// Using the client for s1 should work for s2 now since the listener was passed along
+	assertResp(t, s1.URL, c1, "msg3")
+
+	// Hanging server1 request should still be service-able even after s2 has taken over
+	server1Msgs <- "msg2"
+	<-msg2Response
+}
+
+func assertResp(t *testing.T, url string, c *http.Client, expected string) {
+	resp, err := c.Get(url)
+	if err != nil {
+		t.Fatalf("error using test server 1: %v", err)
+	}
+	respData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("error reading body: %v", err)
+	}
+	if expected != string(respData) {
+		t.Fatalf("expected %s, got %s", expected, string(respData))
+	}
+}
+
+func createTestServer(t *testing.T, pid int, coordDir string, requests chan<- struct{}, responses <-chan string) (*Upgrader, *httptest.Server) {
+	server := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		t.Logf("server %d got a request", pid)
+		// Let the test harness know a client is waiting on us
+		requests <- struct{}{}
+		// And now respond, as requested by the test harness
+		resp := <-responses
+		w.Write([]byte(resp))
+	}))
+
+	upg, err := newUpgrader(mockOS{pid: pid}, coordDir, WithLogger(l))
+	if err != nil {
+		t.Fatalf("error creating upgrader: %v", err)
+	}
+
+	l, err := upg.Fds.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("unable to listen: %v", err)
+	}
+	server.Listener = l
+	server.Start()
+	if err := upg.Ready(); err != nil {
+		t.Fatalf("unable to mark self as ready: %v", err)
+	}
+	return upg, server
+}
+
+type mockOS struct {
+	pid int
+}
+
+func (m mockOS) Getpid() int {
+	return m.pid
+}
+
+func (m mockOS) FindProcess(pid int) (processIface, error) {
+	return mockProcess{nil}, nil
+}
+
+type mockProcess struct {
+	err error
+}
+
+func (m mockProcess) Signal(s os.Signal) error {
+	return m.err
+}
