e9b524f311b66e26ccf2e8b13601e08d19ff9072
Euan Kemp <euan@ngrok.com>
2019-02-26 16:27:51 -0800

    Fix go1.12 dup impl

--- dup_file.go
+++ dup_file.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func dupFile(fh *os.File, name fileName) (*file, error) {
+func dupFile(fh *os.File, name string) (*file, error) {
 	// os.File implements syscall.Conn from go 1.12
 	return dupConn(fh, name)
 }
