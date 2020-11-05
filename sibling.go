502ed0681f8b7c05350dbe1f502445c248316700
Euan Kemp <euank@euank.com>
2020-11-05 18:57:23 +0000

    Remove unneeded dup
    I was hitting an unrelated issue, and logic'd myself into this incorrect
    dup. The dup isn't needed because closing our file doesn't impact the
    send fd the new process has adopted.

--- sibling.go
+++ sibling.go
@@ -77,12 +77,7 @@ func (s *sibling) giveFDs(readyTimeoutC <-chan time.Time, passedFiles map[string
 
 	// Write all files it's expecting
 	for _, fi := range validFds {
-		// dup the file we're sending since we'll eventually close 'fi.file'
-		dup, err := dupFd(fi.file.fd, fi.file.Name())
-		if err != nil {
-			return fmt.Errorf("could not write fds to sibling: %v", err)
-		}
-		if err := sendFile(connFile, dup); err != nil {
+		if err := sendFile(connFile, fi.file); err != nil {
 			return fmt.Errorf("could not write fds to sibling: %v", err)
 		}
 	}
